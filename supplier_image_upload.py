1821214db250a2f0247d8d35e1b57a7c5d880c56
duanH <aces34k@hotmail.com>
2020-12-27 11:27:49 +0800

    2. Uploading images to web server

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -7,8 +7,9 @@ import os
 
 url = "http://localhost/upload/"
 path = "supplier-data/images/"
+images = os.listdir(path)
 
-for image in os.listdir(path)
+for image in images
   if image.endswith(".jpeg"):
     with open(path + image, 'rb') as opened:
       r = requests.post(url, files={'file': opened})
